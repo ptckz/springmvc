b6d434374001491ec64f43ab941bf43311d812e8
Patrick Antonioli <patrickantonioliw@totvs.com.br>
2018-12-14 14:21:58 -0200

    up src

---
+++ src/main/java/com/example/springmvc/repositories/AccountRepository.java
@@ -0,0 +1,17 @@
+package com.example.springmvc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.springmvc.models.Account;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+	Account findOneByEmail(String email);
+
+	@Query("select count(a) > 0 from Account a where a.email = :email")
+	boolean exists(@Param("email") String email);
+}
